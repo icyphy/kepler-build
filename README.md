a55c648ca80e0d04534147dc98a3fe4664b037ff
cxbrooks <cxh@eecs.berkeley.edu>
2018-03-26 15:50:33 -0700

    Build Kepler.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kepler-build
+
 Build Kepler - the scientific workflow system
+
+Kepler (https://kepler-project.org) uses Ptolemy II, so we build it
+here for testing.
---
+++ build-kepler.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+svn co https://code.kepler-project.org/code/kepler/trunk/modules/build-area
+
+#ANT_HOME=/usr/local/apache-ant
+#JAVA_HOME=/usr/java/jdk1.8.0_65
+#PATH=${JAVA_HOME}/bin:/usr/local/bin:${PATH}
+
+cd build-area
+echo "JAVA_HOME: $JAVA_HOME"
+echo "ANT_HOME: $ANT_HOME"
+echo $PATH
+which java
+
+# Get any updates to the build area.
+svn update
+/usr/local/bin/ant change-to -Dsuite=nightly
+/usr/local/bin/ant update
+/usr/local/bin/ant clean
+/usr/local/bin/ant compile eclipse netbeans idea
+
+which javadoc
+/usr/local/bin/ant javadoc
+
+export KEPLER_VERSION=2.4.0.1
+
+# Remove the installers directory, the mac installer requires this.
+rm -rf ../finished-kepler-installers
+
+# Build the linux and windows installers.
+ant make-linux-installer -Dversion=$KEPLER_VERSION
+ant make-windows-installer -Dversion=$KEPLER_VERSION
+
+# Copy the linux and windows installers.
+#cp /var/lib/jenkins/workspace/finished-kepler-installers/windows/Kepler-$KEPLER_VERSION-win.exe /var/lib/jenkins/workspace/kepler/installers/
+
+#cp /var/lib/jenkins/workspace/finished-kepler-installers/linux/Kepler-$KEPLER_VERSION-linux.tar.gz /var/lib/jenkins/workspace/kepler/installers/
